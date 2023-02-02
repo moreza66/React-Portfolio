12f60141d0d0a8bf889bafc938cc490c88c69dfb
mohammadreza memar <mohammadrezamemar@mohammadrezas-MacBook-Pro.local>
2023-02-01 19:02:39 -0600

    add script

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Header from './components/Header';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
+
 const App = () => {
   return (
     <>
@@ -13,5 +15,24 @@ const App = () => {
   </>
   );
 }
+
+
+// const express = require('express')
+// const app = express();
+
+// const path = require('path');
+// const port = process.env.PORT || 5000;
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static('build'));
+//   app.get ('*', (req, res) => {
+//     req.sendfile(path.resolve(__dirname, 'build', 'index.html'));
+//   })
+// }
+
+// app.listen(port, (err) => {
+//   if (err) return console.log(err);
+//   console.log('Server Running', port);
+// })
+
 
 export default App;
